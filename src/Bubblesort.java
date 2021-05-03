6ba108eed0ff8b2142db9120e97261651feef4c3
jona.hollstein <jhollstein@teracortex.com>
2021-05-03 07:38:32 +0200

    update class files

--- src/Bubblesort.java
+++ src/Bubblesort.java
@@ -1,22 +1,19 @@
 public class Bubblesort {
     int smaller;
     int bigger;
-    boolean run = true;
 
     public int[][] sort(int[][] unsorted) {
         for (int j = 0; j < unsorted[0].length; j++) {
-            for (int i = 0; i < unsorted.length && run; i++) {
+            for (int i = 0; i < unsorted.length; i++) {
                 for (int y = 0; y < unsorted.length - 1; y++) {
                     if (unsorted[y][j] > unsorted[y + 1][j]) {
                         bigger = unsorted[y][j];
                         smaller = unsorted[y + 1][j];
                         unsorted[y][j] = smaller;
                         unsorted[y + 1][j] = bigger;
-                        //run = true;
                     }
                 }
             }
-            //run = true;
         }
 
         return unsorted;
