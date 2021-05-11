c56266b6a0243dbd8f224d9d1c7561bed01f090c
jona.hollstein <jhollstein@teracortex.com>
2021-05-11 12:20:41 +0200

    Updated Documentation, removed unessesary code

--- CleanKasiski3.java
+++ CleanKasiski3.java
@@ -6,8 +6,15 @@ import java.text.DecimalFormat;
 import java.util.*;
 
 public class CleanKasiski3 {
-    
-    // Bestimmen der Schlüsselwortlänge vom String des verschlüsselten Texts
+    /*
+    / Find re-occuring strings in the encrypted text
+    / @param String cipher -> encrypted text
+    / @param int min -> min length of repeating characters
+    / @param int max -> max length of repeating characters
+    / @return Arraylist 'distance', storing distances as int
+    */
+
+    //Finn, letzes Jahr
     public static ArrayList<Integer> findRepeats(String cipher, int min, int max) {
         // initalisieren und definieren einer neuen ArrayList als Integer-Typ
         ArrayList<Integer> distance = new ArrayList<>();     
@@ -20,7 +27,7 @@ public class CleanKasiski3 {
                 String s = cipher.substring(spos, spos + l);
                 // Herausfiltern der gef. Buchstabenketten
                 for (int i = spos + l; i < cipher.length(); i++) {      
-                    // Herausfinden der Abstände für Buchstabenwiederholungen mit Hochzählen einer for-Schleife
+                    // storing a value in ArrayList 'distance', if 'distance' does not already contain the value
                     if (cipher.startsWith(s, i)) {
                         if (!distance.contains(i - spos)) {
                             distance.add(i - spos);
@@ -34,6 +41,7 @@ public class CleanKasiski3 {
 
 
     // calculate keyword length
+    // Finn, letztes Jahr, angepasst durch Jona
     public static int calcKeywordlength(ArrayList<Integer> distance) {
         int ggt = distance.get(0); // größter gemeinsamer Teiler
         for (int i = 1; i < distance.size(); i++) { 
@@ -44,16 +52,18 @@ public class CleanKasiski3 {
     }
 
     /* Ermitteln des größten gemeinsamen Teilers
-    *@param int a
-    *param int b
+    * @param int a
+    * @param int b
+    * recursive!!
     */
+    // Finn, letztes Jahr, angepasst durch Jona
     public static int getGGT(int a, int b) {
         if (b == 0) {
             return a;
         }
         if (b > a) {
             return getGGT(b, a);
-        }
+        } // math: modulo operation. divide a/b and return remainder. | ex: 19 % 5 = 4 |
         return getGGT(a % b, b);
     }
 
@@ -62,6 +72,7 @@ public class CleanKasiski3 {
     *@param String cipher - Geheimtext
     *@param int keywordlength - Schlüssellänge
     */
+    // Jona, neu geschrieben auf Basis von Finns Struktur (Troubleshooting mit Moritz, Danke!!)
     public static int[][] calcFreqAbs(String cipher, int keywordlength) {
         //new two-dimensional int array for char frequency at keyword position
         int[][] freqAbs = new int[26][keywordlength];
@@ -69,21 +80,24 @@ public class CleanKasiski3 {
         //iterate over every keyword position
         for (int keypos = 0; keypos < keywordlength; keypos++) {
             //iterate over every char at block position
+            // i = i + keywordlength
             for (int i = keypos; i < cipher.length(); i += keywordlength) {
                 //increase number of char for detected chat at key position
                 freqAbs[(cipher.charAt(i) - 65)][keypos]++;
             }
         }
-
         return freqAbs;
     }
 
     /*
-    *calculate frequency relative
+    * calculate frequency relative
+    * @param int[][] freqAbs -> absolute count of each character on each keyword position
+    * @param Sting cipher -> encrypted string 
+    * @param int keywordlength -> the length of the keyword, as calculated above
     */
-
+    // Jona, neu geschrieben auf Basis von Finns Struktur
     public static double[][] calcFreqRel(int[][] freqAbs, String cipher, int keywordlength){
-        //new double block initialize with block length
+        //new double 'block' initialize with block length
         double block = ((double)cipher.length()/keywordlength);
         //new two-dimensional int array for the relative frequency
         double[][] freqRel = new double[26][freqAbs[0].length];
@@ -97,7 +111,9 @@ public class CleanKasiski3 {
         }
         return freqRel ;
     }
+
     // Print most frequent letter with freqRel
+    // Jona
     public static void printTop(double[][] freqRel){
         //initialize two arrays to store the value and index of the most frequent char
         double[] first = new double[freqRel[0].length];
@@ -116,50 +132,49 @@ public class CleanKasiski3 {
                     //store current index + 65 (ASCII...)
                     firstpos[j] = i+65;
                 }
-            }
-        }
-        for(int i = 0; i < freqRel.length; i++){
-            //iterate Z Axis (all Keyword positions)
-            for(int j = 0; j <freqRel[i].length; j++){
-                //store most frequent letter for every keyword position
-                if(freqRel[i][j] > second[j] && freqRel[i][j] < first[j]){
+                else if(freqRel[i][j] > second[j] && freqRel[i][j] < first[j]){
                     second[j] = freqRel[i][j];
                     secondpos[j] = i+65;
                 }
             }
         }
+        
         // initialize DecimalFomat with patten 00.00
         DecimalFormat df = new DecimalFormat("00.00");
         //print both arrays
         for(int l = 0; l < freqRel[0].length; l++){
             // for each row:  index + 1      Most frequent character        value                  Second most frequent character     value
-            System.out.println((l+1) + ": " + (char)firstpos[l] + " | " + df.format(first[l]) + " || " + (char)secondpos[l] + " | " + df.format(second[l]));
+            System.out.println((l+1) + ": " + (char)firstpos[l] + " | " + df.format(first[l]) +      " || " + (char)secondpos[l] + " | " + df.format(second[l]));
         }
     }
 
     // Häufkeitsverteilung der Sprache printen
+    // Jona
     public static void printFreq(double[] language) {
-        System.out.print("\n" + "Häufigkeit: | ");
+        DecimalFormat df = new DecimalFormat("00.00");
+        System.out.print("| ");
+        // enhanced for-each loop: iterate through each position in the array
         for (double i : language) {
-            System.out.print(i + " | ");
+            //format to "00.00"
+            String formatted = df.format(i);
+            formatted = formatted.replaceAll("\\A0", " ");
+            //print formatted String + |
+            System.out.print(formatted + "|");
         }
 
-        /* 'Enhanced' foreach-Schleife
-
-        for (int i = 0; i < language.length; i++) {
-            System.out.print(language[i] + " | ");
-        }*/
         
         //print new line and "Zeichen    |   "
-        System.out.print("\n" + "Zeichen:    |   ");
+        System.out.print("\n" + "|   ");
         //iterate over every char in the alphabet
         for (char c = 'A'; c <= 'Z'; c++) {
             //print out current char and "  |  "
-            System.out.print(c + "  |   ");
+            System.out.print(c + "  |  ");
         }
     }
     // Alle Häufigkeiten printen (optional)
+    // Jona
     public static void printAllFreq(int[][] freqAbs, double[][] freqRel, int keywordlength){
+        //initialize 2 DecimalFormat objects
         DecimalFormat df = new DecimalFormat("00.00");
         DecimalFormat cf = new DecimalFormat("00");
 
@@ -169,7 +184,7 @@ public class CleanKasiski3 {
         //iterate over the alphabet
         for (int i = 0; i < 26; i++) {
             //print current char and divider
-            System.out.print(c + " | ");
+            System.out.print(c + " |");
             //iterate over every keyword position
             for (int j = 0; j < keywordlength; j++) {
                 System.out.print(cf.format(freqAbs[i][j]) + " | ");
@@ -185,7 +200,7 @@ public class CleanKasiski3 {
         //iterate over the alphabet
         for (int i = 0; i < 26; i++) {
             //print current char and divider
-            System.out.print(c + " | ");
+            System.out.print(c + " |");
             //iterate over every keyword position
             for (int j = 0; j < keywordlength; j++) {
                 System.out.print(df.format(freqRel[i][j]) + " | ");
@@ -195,7 +210,7 @@ public class CleanKasiski3 {
         }
 
     }
-
+    // erste 5 Zeilen: Finn, Rest Jona
     public static void main(String[] args) {
         //initialize string cipher wirh decrypted text
         String cipher = "PWTMYTBADKDGPWPFYWFGUESOTLUPNVYWAPKCSOOJWWASTLSUZUSJMJBBRSTIMGPYSXOJWWASMMZQLCHJQWGYDHKOJWWASTMFPADWIPVKLHONZWPDPWRAAGQPRKNJCNPKGPJJLTHYOWOHPGYJWCUEKUZLGAOWKHOGPESMZMRWPBKVFVZTQNLAGSFSMVWTDPWRAAGQPRKNJCNPTGTKEOMSGVLYVCHKBVKLOFOBLGNCIVXWPLYBZAAEOOWKEWEODZKZOGPWGOMSWMPWTIFFLCTUTYGUOSLZSILYOHEWEODSRVVYHSFAVVHHWGIPTGHYHCWJVLERGJWKPDHGJWTUTQNBXGZEUKTWIAZPPMOGPWGJQWGYDHKNJCNPSOVWTZPFOMNQUQFGOWPYTQNBAIVOSXNSNZNVHMSPAHCXBWVDTFJRWFLASXAGPHYHCWJVLEOANWKUPTXIYGUFFSQLLHZRKZFGPYTXIYGUOWKVAEOEAOBBCVOSXVWKUMSGVLYVCHKBOGYOSTSGGUYSTAAPKYWIPLBBRSRIKULYJUVWKUPFHMDKLMWMMFRLCGUVKQSWAGVVWYNVLZSILYROMKKJSBAZSWMOWKHMILSCKZAIRPWZHMGPYSXLWTNCIVXWPIPNOMZGUSSXIMUIPYUUEGUKICMDEOPFMZMRWPGOMYGOZSXBOKLGWKTWHYLUKVEWZDAGVEKUOSYBWPZDHKTDGUFBJEWNJSSLZSILYYUMFPAPAGVKVLWZKV";
@@ -213,15 +228,17 @@ public class CleanKasiski3 {
         double[][] freqRel = calcFreqRel(freqAbs, cipher, keywordlength);
 
         //print distances and keywordlength
+        System.out.println("\nHäufigkeitsverteilung im Deutschen Alphabet:");
         printFreq(DEUTSCH);
         
         System.out.println("\n \n" + "Gefundene Abstände: " + distance);
         System.out.println("\n" + "Schlüsselwortlänge: " + keywordlength);
 
+        //print most frequent characters
         System.out.println("\n" + "Häufigster Buchstabe an der Schlüsselposition:");
         printTop(freqRel);
 
-        //System.out.println("\nHaufigkeiten an Blockpositionen");
-        //printAllFreq(freqAbs, freqRel, keywordlength);
+        System.out.println("\nHaufigkeiten an Blockpositionen");
+        printAllFreq(freqAbs, freqRel, keywordlength);
     }
 }
